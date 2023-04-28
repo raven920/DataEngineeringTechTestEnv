3eb8446ce3f755c5a790f682b063b5a4ed8f848a
raven920 <raven@ideashock.net>
2023-04-28 09:04:58 -0500

    changing folder structure

--- scripts/run-jupyter.sh
+++ scripts/run-jupyter.sh
@@ -5,6 +5,4 @@ export JUPYTER_FINAL_PASS=$(python3 -c "from notebook.auth import passwd; print(
 #echo $JUPYTER_FINAL_PASS > /home/jupyter/pass
 #echo 'export SPARK_HOME="/opt/spark"; export PATH="$SPARK_HOME/bin:$PATH"; export PYSPARK_DRIVER_PYTHON="jupyter"; export PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.password=\"$JUPYTER_FINAL_PASS\" --notebook-dir=/home/jupyter/notebooks --no-browser --ip 0.0.0.0 --port=8080 --allow-root"; pyspark' > /home/jupyter/sparkexecution
 
-
-cd ~/
 screen -dmS jupyter bash -c 'export SPARK_HOME="/opt/spark"; export PATH="$SPARK_HOME/bin:$PATH"; export PYSPARK_DRIVER_PYTHON="jupyter"; export PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.password=\"$JUPYTER_FINAL_PASS\" --notebook-dir=/home/jupyter/jupyter_workspace --no-browser --ip 0.0.0.0 --port=8080 --allow-root"; pyspark'

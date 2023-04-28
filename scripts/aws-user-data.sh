debdfbf1f7d3486a67e2ac80c020f9b7cfa5420d
raven920 <raven@ideashock.net>
2023-04-28 07:57:09 -0500

    added sample datasets

--- scripts/aws-user-data.sh
+++ scripts/aws-user-data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 cd ~/
 dnf install -y git iptables
 git clone https://github.com/raven920/DataEngineeringTechTestEnv.git install_files
@@ -8,5 +9,5 @@ adduser jupyter
 cp install_files/scripts/run-jupyter.sh /home/jupyter/run-jupyter.sh
 
 chmod a+x /home/jupyter/run-jupyter.sh
-sudo bash TechnicalTestEnvironment/scripts/install-spark.sh
+sudo bash install_files/scripts/install-spark.sh
 sudo su -c "bash /home/jupyter/run-jupyter.sh ${JUPYTER_PWORD}" jupyter
